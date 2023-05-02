36750f2cd2a7015f136b125389bdbb887590dcef
0xDevCookie <ssssrxdd+11@gmail.com>
2023-05-01 22:13:21 -0400

    bull shit

--- config/site.ts
+++ config/site.ts
@@ -1,3 +1,10 @@
+import {
+  UploadCloud,
+  Lock,
+  RefreshCcw,
+  Fingerprint
+} from "lucide-react"
+
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
@@ -41,5 +48,36 @@ export const siteConfig = {
       { name: 'Privacy', href: '#' },
       { name: 'Terms', href: '#' },
     ],
-  }
+  },
+  stats:[
+    { label: 'Transactions every 24 hours', value: '44 million' },
+    { label: 'Assets under holding', value: '$119 trillion' },
+    { label: 'New users annually', value: '46,000' },
+  ],
+  features: [
+    {
+      name: 'Push to deploy',
+      description:
+        'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+      icon: UploadCloud,
+    },
+    {
+      name: 'SSL certificates',
+      description:
+        'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+      icon: Lock,
+    },
+    {
+      name: 'Simple queues',
+      description:
+        'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+      icon: RefreshCcw,
+    },
+    {
+      name: 'Advanced security',
+      description:
+        'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+      icon: Fingerprint,
+    },
+  ]
 }
